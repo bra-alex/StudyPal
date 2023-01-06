3b2c8117dfb0f022b3d96dc30cb50ff7a207cb75
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-01-06 20:45:51 +0000

    Add user model and schema

--- server/models/forum/posts/posts.mongo.js
+++ server/models/forum/posts/posts.mongo.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const postSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
         required: true
     },
     postContent: {
---
+++ server/models/users/users.model.js
@@ -0,0 +1,70 @@
+const User = require('./users.mongo')
+
+async function getAllUsers(){
+    try {
+        return await User.find({}, {
+            'posts': 0,
+            '__v': 0
+        })
+    } catch (e) {
+        console.log(e)
+        e.status = 500
+        e.message = 'Error getting users from the database'
+        throw e
+    }
+}
+
+async function getUser(uid){
+    try {
+        return await User.find({uid: uid}, {
+            '__v': 0
+        })
+    } catch (e) {
+        console.log(e)
+        e.status = 500
+        e.message = 'Error getting user from the database'
+        throw e
+    }
+}
+
+async function createUser(user){
+    try{
+        const newUser = new User(user)
+        return await newUser.save()
+    } catch(e) {
+        console.log(e)
+        e.status = 500
+        e.message = 'Error creating user'
+        throw e
+    }
+}
+
+async function updateUser(user){
+    try {
+        return await User.findOneAndUpdate({uid: uid}, user)
+    } catch (e) {
+        console.log(e)
+        e.status = 500
+        e.message = 'Error updating user info'
+        throw e
+    }
+}
+
+async function deleteUser(uid){
+    try {
+        return await User.findOneAndDelete({uid: uid})
+    } catch (e) {
+        console.log(e)
+        e.status = 500
+        e.message = 'Error deleting user from the database'
+        throw e
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+}

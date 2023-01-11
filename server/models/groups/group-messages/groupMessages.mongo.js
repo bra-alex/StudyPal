4cf83a72cdb215f867202f42c0b75edb1f76898f
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-01-11 23:55:03 +0000

    Update group messages schema and add model.

--- server/models/groups/group-messages/groupMessages.mongo.js
+++ server/models/groups/group-messages/groupMessages.mongo.js
@@ -3,42 +3,23 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const groupMessagesSchema = new Schema({
-    sender: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    group: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Group',
-        required: true
-    },
     messages: [
         {
-            recipients: [
-                {
-                    type: mongoose.Types.ObjectId,
-                    ref: 'User',
-                    required: true
-                }
-            ],
-            messages: [
-                {
-                    message: {
-                        type: String,
-                        required: true
-                    },
-                    sender: {
-                        type: mongoose.Types.ObjectId,
-                        ref: 'User',
-                        required: true
-                    },
-                    date: {
-                        type: Date,
-                        default: Date.now()
-                    }
-                }
-            ]
+            message: {
+                type: String,
+                required: true
+            },
+            sender: {
+                type: mongoose.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            date: {
+                type: Date,
+                default: Date.now()
+            }
+
+
         }
     ]
 })

50599c6f364dee4d2272c3c0b8dd3c51d2cc471c
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-01-17 13:55:26 +0000

    Update user schema

--- server/models/users/users.mongo.js
+++ server/models/users/users.mongo.js
@@ -19,6 +19,10 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    password: {
+        type: String,
+        required: true
+    },
     profileImageUrl: {
         type: String,
         required: true
